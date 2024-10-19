b01c2e912d660dde733608f7c17399773147d8d5
Cleberson <Silva>
2024-10-19 12:13:20 -0300

    refatorando o exercicio 02

--- exercicio_02.py
+++ exercicio_02.py
@@ -1,6 +1,24 @@
 # Criar um programa onde o usuário digite dois valores e apareça a soma
 
-valor1 = int(input("Digite um valor: "))
-valor2 = int(input("Digite Outro  valor: "))
+#valor1 = int(input("Digite um valor: "))
+#valor2 = int(input("Digite Outro  valor: "))
 
-print(valor1 + valor2)
+#print(valor1 + valor2)
+
+#numero = 3 # int
+#print(numero)
+# imprime a tipagem da variavel
+#print(type(numero))
+
+#numero = 4 
+#print(numero)
+
+#numero = "Python"
+#print(numero)
+
+
+# Exercicio 3: refatorando o programa 02
+nome = input("Digite seu nome: ")
+quantidade_caracteres = len(nome)
+print("Qual é o seu nome? " + nome)
+print("Total de Caracteres: " + str(quantidade_caracteres))
