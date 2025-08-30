e9cbb9727f40138dc35eefb285312bbc22863bff
Cleberson <Silva>
2025-08-30 17:20:15 -0300

    Resolvendo conflito

--- kpy.py
+++ kpy.py
@@ -1,10 +1,10 @@
 #Escreva um programa em Python que solicita ao usuário para digitar seu nome, o valor do seu salário mensal e o valor do bônus que recebeu. O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome e informando o valor do salário em comparação com o bônus recebido.
-
+CONSTANTE_BONUS = 1000
 
 nome = input("Digite seu nome: ")
 salario = float(input ("Informe o seu salario: "))
 bonus = float(input ("Informe a Porcentagem do Bonus: "))
 
-total_bonus = 1000 + (salario * bonus)
+total_bonus = CONSTANTE_BONUS + (salario * bonus)
 
 print(f"Olá {nome}, seu bônus foi de {total_bonus})
